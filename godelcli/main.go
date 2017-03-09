b40f8f059cd9a20f9b0084fdedb50ced781413cb
Randy Horman <rwhorman@gmail.com>
2017-03-09 18:14:40 -0500

    more refactoring ... move root cmd into main

--- godelcli/main.go
+++ godelcli/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"godel/cmd"
+)
+
 func main() {
 	rootCmd.Run()
+}
+
+var rootCmd = cmd.Command{
+	Name:    "godel",
+	Handler: nil,
+	Flags:   nil,
+	UsageHelp: `Command-line interface to the Gödel system.
+
+Provides a means to interact with the Gödel system from a terminal
+session, or enables scripted automation of various aspects
+and behaviours of the Gödel system.
+
+The Gödel system is a mesh network of devices comprising an automation
+environment for the betterment of lifestyle.`,
+	Description: "Command-line interface to the Gödel system",
+	Examples: `# Retrieve all the thingys
+godel get ... `,
 }
