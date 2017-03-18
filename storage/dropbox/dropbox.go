ca69649b99ff8c3a1585e2524e3f176fff931e02
Randy Horman <rwhorman@gmail.com>
2017-03-17 21:31:00 -0400

    Beginnings of a storage plugin framework, with skeleton dropbox plugin

---
+++ storage/dropbox/dropbox.go
@@ -0,0 +1,21 @@
+package dropbox
+
+import (
+	"net/http"
+)
+
+const (
+	AccessToken string = "redacted"
+)
+
+type DropboxStorageClient struct {
+}
+
+func (d *DropboxStorageClient) GetFile(path string) ([]byte, error) {
+	// curl https://content.dropboxapi.com/2/files/download --header 'Authorization: Bearer <access token>' --header 'Dropbox-API-Arg: {"path":"/<file path>"}'
+	return []byte{"filecontents"}, nil
+}
+
+func NewClient() *DropboxStorageClient {
+	return &DropboxStorageClient{}
+}
